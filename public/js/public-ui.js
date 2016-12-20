e2c6f9dbc6ed692e489f4b40bb6fc7872cc4ef43
hansm83 <aioie@naver.com>
2016-12-20 17:23:16 +0900

    20161220 modify

--- public/js/public-ui.js
+++ public/js/public-ui.js
@@ -82,6 +82,16 @@
 			addClassCheckBox($(this));
 		})
 		
+		//select
+		$(function() {
+		  var selectTarget = $('.select_type select');
+
+		  selectTarget.change(function() {
+			var select_name = $(this).children('option:selected').text();
+			$(this).siblings('label').text(select_name);
+		  });
+		});
+		
 	});
 	
 })(this,this.jQuery);
